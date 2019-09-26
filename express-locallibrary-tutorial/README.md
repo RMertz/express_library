78fd0ac85751217ad6911343a149fcd1ece5b646
RMertz <antonmertz@gmail.com>
2019-09-26 17:25:32 -0600

    Complete Section 3 of tutorial

---
+++ express-locallibrary-tutorial/README.md
@@ -0,0 +1,9 @@
+database connection: 
+
+mongodb+srv://antonmertz:<password>@cluster0-phpry.mongodb.net/test?retryWrites=true&w=majority
+
+
+
+updated:
+
+mongodb+srv://antonmertz:Dash2013!@cluster0-phpry.mongodb.net/local_library?retryWrites=true&w=majority
--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -10,6 +10,14 @@ var coolRouter = require('./routes/cool');
 
 var app = express();
 
+//mongodb connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb+srv://antonmertz:Dash2013!@cluster0-phpry.mongodb.net/local_library?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
