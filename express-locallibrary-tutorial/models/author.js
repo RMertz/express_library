a3fd9b52c966d4ac423b564a66c6341b77e1cfd9
RMertz <antonmertz@gmail.com>
2019-09-27 12:53:31 -0600

    Add basic list pages for genre, author, book, book_instance

--- express-locallibrary-tutorial/models/author.js
+++ express-locallibrary-tutorial/models/author.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var moment = require('moment');
 
 var Schema = mongoose.Schema;
 
@@ -25,5 +26,19 @@ AuthorSchema
 	return '/catalog/author/' + this._id;
 });
 
+// Virtual for date_of_birth_formatted
+AuthorSchema
+.virtual('date_of_birth_formatted')
+.get(function () {
+				return moment(this.date_of_birth).format('MMMM Do, YYYY');
+});
+
+// Virtual for date_of_death_formatted
+AuthorSchema
+.virtual('date_of_death_formatted')
+.get(function () {
+				return this.date_of_death ?
+				moment(this.date_of_death).format('MMMM Do, YYYY') : '';
+});
 //Export model
 module.exports = mongoose.model('Author', AuthorSchema);
